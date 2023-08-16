680097f9fda5e5ee638e1084474a862c122bba2e
Nesline DAlmeida <neslinedalmeida@gmail.com>
2023-08-16 14:58:07 +0530

    Done

---
+++ src/main/java/com/driver/classA.java
@@ -0,0 +1,7 @@
+package com.driver;
+
+public class classA {
+    public String meth(){
+        return "Invoking method from class A";
+    }
+}
---
+++ src/main/java/com/driver/classB.java
@@ -0,0 +1,8 @@
+package com.driver;
+
+public class classB extends classA{
+    @Override
+    public String meth() {
+        return "Method is overridden in Extendend class B";
+    }
+}
